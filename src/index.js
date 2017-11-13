ba156c7eb415e303ee6eecbdfbeaa9e490cf5627
Gerson Calienes <gersoncalienes@gmail.com>
2017-11-12 20:18:37 -0500

    added provider to index and added the redirects page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from "react-redux";  //allows us to provide the store to all the compoents that make up the app
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>    
+        <AppRouter /> 
+    </Provider>,
+    document.getElementById('root'));
+
 registerServiceWorker();
