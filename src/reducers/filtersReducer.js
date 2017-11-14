3fab8583030af4563abc55b3bff34697296aa65b
Gerson Calienes <gersoncalienes@gmail.com>
2017-11-14 12:03:20 -0500

    added the filters reducer

---
+++ src/reducers/filtersReducer.js
@@ -0,0 +1,43 @@
+import moment from'moment';
+
+/////////////////REDUCERS////////////////////////////////REDUCERS/////////////////////////////
+/////////////////////////////////////FILTER REDUCERS/////////////////////////////////////////////
+const filtersReducerDefaultState = { //filters is not an array just an object, not like the expenses(multiple expenses)
+    text: '', 
+    sortBy: 'date', //value can change from date or amount
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+};
+
+export default ( state= filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        case 'SORT_BY_DATE':
+            return {
+                ...state, //we want to spread out the state which by default is filtersReducerDefaultState and update it with the next item
+                sortBy: 'date'
+            };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate:action.endDate
+            }
+        default:
+        return state;
+    }
+};
+//////////////////////END OF REDUCERS///////\\\\\\\\\\\\\\\\\\\\\\\\\
