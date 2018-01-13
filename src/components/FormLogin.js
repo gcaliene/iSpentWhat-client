69cab3ac48a5f29ce2dae707e1a0facf3d1b6a60
Unknown <gersoncalienes@gmail.com>
2018-01-13 18:19:43 -0500

    Updated the css for the inputs. Now they are larger.

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import { loginUser } from '../actions/action';
+import { loginUser } from "../actions/action";
 
 class FormLogin extends Component {
-  handleSubmit(event) {
-    event.preventDefault();
-    // console.log('d');
-    const username = event.target.username.value;
-    const password = event.target.password.value;
-    this.props.dispatch(loginUser(username, password));
-  }
+	handleSubmit(event) {
+		event.preventDefault();
+		// console.log('d');
+		const username = event.target.username.value;
+		const password = event.target.password.value;
+		this.props.dispatch(loginUser(username, password));
+	}
 
-  render() {
-    return (
-      <form
-        onSubmit={e => this.handleSubmit(e)}
-        className="FormRegister-container-form"
-      >
-        <br />
-        <p
-          id="error-username-login"
-          className=" class-success-username-login hidden"
-        />
-        <input type="text" name="username" placeholder="username" required />
-        <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="password"
-          required
-        />
-        <br /> <br />
-        <button>Login</button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form
+				onSubmit={e => this.handleSubmit(e)}
+				className="FormRegister-container-form"
+			>
+				<br />
+				<p
+					id="error-username-login"
+					className=" class-success-username-login hidden"
+				/>
+				<input type="text" name="username" placeholder="Username" required />
+				<br />
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					required
+				/>
+				<br /> <br />
+				<button>Login</button>
+			</form>
+		);
+	}
 }
 
 export default connect()(withRouter(FormLogin));
